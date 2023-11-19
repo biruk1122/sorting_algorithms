588bb93ffd729acc6591608d003b82fad0212c12
biruk1122 <birukfikadu29@gmail.com>
2023-11-19 03:05:40 -0800

    updated

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,19 +15,18 @@ void bubble_sort(int *array, size_t size)
     for (i = 0; i < size - 1; i++)
     {
         swapped = 0;
-
-        for (j = size - 1; j > i; j--)
+        for (j = 0; j < size - 1 - i; j++)
         {
-            if (array[j] < array[j - 1])
+            if (array[j] > array[j + 1])
             {
                 temp = array[j];
-                array[j] = array[j - 1];
-                array[j - 1] = temp;
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
                 swapped = 1;
                 print_array(array, size);
             }
         }
-	if (swapped == 0)
+        if (swapped == 0)
             break;
     }
 }
